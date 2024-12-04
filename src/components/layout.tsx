0a91271898bf4be29d08a7ae5752d8420d179513
Kuroneko <ken.aster8@gmail.com>
2024-12-05 07:59:11 +0900

    レイアウトコンポーネントにCSSを追加

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,10 @@
+import styles from "./layout.module.css";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className={styles.container}>
       <h2>Next.js Practice</h2>
       {children}
-    </>
+    </div>
   );
 }

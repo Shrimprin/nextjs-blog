f40bf13c44c60c03e15defb44112ac3defe9d94a
Kuroneko <ken.aster8@gmail.com>
2024-12-13 12:24:40 +0900

    画像がコンテナに収まるように修正した

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -6,7 +6,13 @@ const FirstPost: NextPage = () => {
   return (
     <>
       <h1>Hello, first post!</h1>
-      <Image src="/images/neko.jpg" height={555} width={737} alt="猫" />
+      <Image
+        src="/images/neko.jpg"
+        layout="responsive"
+        width={737}
+        height={555}
+        alt="猫"
+      />
       <br />
       <Link href="/">戻る</Link>
     </>

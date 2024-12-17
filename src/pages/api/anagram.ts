4f92d9e916e9e7bbd120f12b8d2ce05df78c4bc8
Kuroneko <ken.aster8@gmail.com>
2024-12-17 22:27:03 +0900

    new Setを使って重複した文字列を排除するようにしてパフォーマンス向上

--- src/pages/api/anagram.ts
+++ src/pages/api/anagram.ts
@@ -6,15 +6,15 @@ export type AnagramResult = {
 
 const generateAnagrams = (str: string): string[] => {
   if (str.length <= 1) return [str];
-  const anagrams: string[] = [];
+  const anagrams = new Set<string>();
   for (let i = 0; i < str.length; i++) {
     const char = str[i];
     const remainingChars = str.slice(0, i) + str.slice(i + 1);
     for (const subAnagram of generateAnagrams(remainingChars)) {
-      anagrams.push(char + subAnagram);
+      anagrams.add(char + subAnagram);
     }
   }
-  return anagrams;
+  return Array.from(anagrams);
 };
 
 export default function handler(
